f3f94eb10963f2c6bbc35f56d32f64c930ee0392
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-09-06 21:20:01 +0200

    create email service

---
+++ src/main/java/com/vasylkorol/ysellb/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.vasylkorol.ysellb.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+}
